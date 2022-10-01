ae56ae5d32ac9c933a4e422f089ce9f9af0e23c9
Kooreis <kooreis.official@gmail.com>
2022-10-01 06:00:00 -0500

    Initial setup of the program. A Stopwatch object and a string variable for user commands are declared.

--- Simple_Stopwatch.cs
+++ Simple_Stopwatch.cs
@@ -7,25 +7,5 @@ class Program
     {
         Stopwatch stopwatch = new Stopwatch();
         string command = "";
-
-        while (command != "exit")
-        {
-            Console.WriteLine("Enter 'start' to start stopwatch, 'stop' to stop, 'reset' to reset, 'exit' to exit");
-            command = Console.ReadLine();
-
-            switch (command)
-            {
-                case "start":
-                    stopwatch.Start();
-                    break;
-                case "stop":
-                    stopwatch.Stop();
-                    Console.WriteLine("Elapsed time: {0}", stopwatch.Elapsed);
-                    break;
-                case "reset":
-                    stopwatch.Reset();
-                    break;
-            }
-        }
     }
 }
