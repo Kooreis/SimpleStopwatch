5c442a5052e5b5bfe6952549a4f18f51d0364254
Kooreis <kooreis.official@gmail.com>
2022-10-01 06:00:00 -0500

    Initial setup of the Stopwatch class with a private variable 'start' to hold the start time. Also, imported the Scanner class for user input.

--- Simple_Stopwatch.java
+++ Simple_Stopwatch.java
@@ -1,34 +1,5 @@
-```java
 import java.util.Scanner;
 
 public class Stopwatch {
     private long start;
-
-    public static void main(String[] args) {
-        Stopwatch stopwatch = new Stopwatch();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Press 's' to start the stopwatch");
-        String input = scanner.nextLine();
-        if (input.equalsIgnoreCase("s")) {
-            stopwatch.start();
-        }
-
-        System.out.println("Press 'e' to end the stopwatch");
-        input = scanner.nextLine();
-        if (input.equalsIgnoreCase("e")) {
-            stopwatch.stop();
-        }
-    }
-
-    public void start() {
-        start = System.currentTimeMillis();
-    }
-
-    public void stop() {
-        long end = System.currentTimeMillis();
-        long elapsedTime = end - start;
-        System.out.println("Elapsed time in milliseconds: " + elapsedTime);
-    }
 }
-```
